29ce995db494d20bd25a1613d88196760d1ef0c0
Leslie Mugodi <mugodileslie@gmail.com>
2025-04-29 18:39:54 +0100

    fix: replace default Expo starter screen with full app logic

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import * as Notifications from 'expo-notifications';
+import { useEffect } from 'react';
+import { Platform } from 'react-native';
+import React from 'react';
+import MainNavigator from './navigation/MainNavigator';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    async function getPermissions() {
+      const { status } = await Notifications.requestPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Notification permission not granted');
+      }
+    }
+  
+    getPermissions();
+  }, []);
+  
+  return <MainNavigator />;
+}
